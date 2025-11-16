833a0e856f6f3882ae1da00232b3a1e106e6ed22
pauleveritt <pauleveritt@me.com>
2025-11-15 22:03:54 -0500

    2025-11-15-refactor: Remove utils module and integrate `get_certain_callable` into nodes.

--- src/storytime/nodes.py
+++ src/storytime/nodes.py
@@ -2,8 +2,52 @@
 
 from dataclasses import dataclass, field
 from importlib import import_module
+from inspect import getmembers, isfunction
 from pathlib import Path
-from typing import Any
+from types import ModuleType
+from typing import TYPE_CHECKING, Any, get_type_hints
+
+if TYPE_CHECKING:
+    from storytime.section import Section
+    from storytime.site import Site
+    from storytime.subject import Subject
+
+
+def get_certain_callable(module: ModuleType) -> "Site | Section | Subject | None":
+    """Return the first Site/Section/Subject in given module that returns correct type.
+
+    A ``stories.py`` file should have a function that, when called,
+    constructs an instance of a Section, Subject, etc. This helper
+    function does the locating and construction. If no function
+    is found with the correct return value, return None.
+
+    We do it this way instead of magically-named functions, meaning,
+    we don't do convention over configuration.
+
+    Args:
+        module: A stories.py module that should have the right function.
+
+    Returns:
+        The Site/Section/Story instance or ``None`` if there wasn't an
+        appropriate function.
+    """
+    from storytime.section import Section
+    from storytime.site import Site
+    from storytime.subject import Subject
+
+    valid_returns = (Site, Section, Subject)
+    for _name, obj in getmembers(module):
+        if isfunction(obj) and obj.__module__ is module.__name__:
+            th = get_type_hints(obj)
+            return_type = th.get("return")
+            if return_type and return_type in valid_returns:
+                # Call the obj to let it construct and return the
+                # Site/Section/Subject
+                target: Site | Section | Subject = obj()
+                return target
+
+    # We didn't find an appropriate callable
+    return None
 
 
 @dataclass()
@@ -28,8 +72,6 @@ class TreeNode:
 
     def __post_init__(self) -> None:
         """Assign calculated fields."""
-        from storytime.utils import get_certain_callable
-
         root_package_path = self._get_root_package_path()
         relative_stories_path = self._get_relative_stories_path(root_package_path)
 
