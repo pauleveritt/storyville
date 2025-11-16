6b5da1b06fd808306f9d335a73f0ee430163e8db
pauleveritt <pauleveritt@me.com>
2025-11-15 19:05:35 -0500

    Fix warning.

--- src/storytime/__init__.py
+++ src/storytime/__init__.py
@@ -7,17 +7,18 @@ You can then browse them in a web page, as well as use these stories in testing.
 
 from pathlib import Path
 
-from storytime.nodes import BaseNode
-from storytime.nodes import TreeNode
+from storytime.nodes import BaseNode, TreeNode
 from storytime.section import Section
-from storytime.site import Site
-from storytime.site import make_site
+from storytime.site import Site, make_site
 from storytime.story import Story
 from storytime.subject import Subject
 from storytime.utils import get_certain_callable
 
+PACKAGE_DIR = Path(__file__).resolve().parent
+
 __all__ = [
     "BaseNode",
+    "PACKAGE_DIR",
     "Section",
     "Site",
     "Story",
@@ -26,5 +27,3 @@ __all__ = [
     "get_certain_callable",
     "make_site",
 ]
-
-PACKAGE_DIR = Path(__file__).resolve().parent
